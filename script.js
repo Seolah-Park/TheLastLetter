5b750b97fe906f8e77ea85fc5f3b0604cb6930b7
Seolah-Park <seolah5@snu.ac.kr>
2024-06-13 07:23:22 +0900

    background

--- script.js
+++ script.js
@@ -30,3 +30,27 @@ function checkMessage() {
 function saveLastMessage(message) {
     localStorage.setItem('lastMessage', message);
 }
+
+
+
+
+
+$(document).ready(function(){
+  var stars=800;
+  var $stars=$(".stars");
+  var r=800;
+  for(var i=0;i<stars;i++){
+    var $star=$("<div/>").addClass("star");
+    $stars.append($star);
+  }
+  $(".star").each(function(){
+    var cur=$(this);
+    var s=0.2+(Math.random()*1);
+    var curR=r+(Math.random()*300);
+    cur.css({ 
+      transformOrigin:"0 0 "+curR+"px",
+      transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
+       
+    })
+  })
+})
