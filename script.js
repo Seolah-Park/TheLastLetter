049a893d180dcc60ca8cbe88c1a6bac039d212ac
Seolah-Park <seolah5@snu.ac.kr>
2024-06-13 01:49:35 +0900

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+function navigate(page) {
+    window.location.href = page;
+}
+
+function showButton() {
+    var input = document.getElementById('codeInput').value;
+    var button = document.getElementById('confirmButton');
+    if (input.trim() !== '') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
