75d7fc1a507a9353776d87378aaa4e9c0280311d
Seolah-Park <seolah5@snu.ac.kr>
2024-06-13 06:32:22 +0900

    svg js

--- script.js
+++ script.js
@@ -1,50 +1,38 @@
-// scripts.js
-window.signature = {
-  initialize: function() {
-    $('.signature svg').each(function() {
-      var paths = $('path, circle, rect', this);
-      var delay = 0;
-      paths.each(function() {
-        var path = $(this);
-        var length = path[0].getTotalLength();
-        var speed = length < 100 ? 20 : Math.floor(length);
-        delay += speed + 100;
-        path.css('transition', 'none')
-            .attr('data-length', length)
-            .attr('data-speed', speed)
-            .attr('data-delay', delay)
-            .attr('stroke-dashoffset', length)
-            .attr('stroke-dasharray', length + ',' + length);
-      });
-    });
-  },
+window.signature =
+  initialize: ->
+    $('.signature svg').each ->
+      paths = $('path, circle, rect', this)
+      delay = 0
+      for path in paths
+        length = path.getTotalLength()
+        previousStrokeLength = speed || 0
+        speed = if length < 100 then 20 else Math.floor(length)
+        delay += previousStrokeLength + 100
+        $(path).css('transition', 'none')
+               .attr('data-length', length)
+               .attr('data-speed', speed)
+               .attr('data-delay', delay)
+               .attr('stroke-dashoffset', length)
+               .attr('stroke-dasharray', length + ',' + length)
 
-  animate: function() {
-    $('.signature svg').each(function() {
-      var paths = $('path, circle, rect', this);
-      paths.each(function() {
-        var path = $(this);
-        var length = path.attr('data-length');
-        var speed = path.attr('data-speed');
-        var delay = path.attr('data-delay');
-        path.css('transition', 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear')
-            .attr('stroke-dashoffset', '0');
-      });
-    });
-  }
-};
+  animate: ->
+    $('.signature svg').each ->
+      paths = $('path, circle, rect', this)
+      for path in paths
+        length = $(path).attr('data-length')
+        speed = $(path).attr('data-speed')
+        delay = $(path).attr('data-delay')
+        $(path).css('transition', 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear')
+               .attr('stroke-dashoffset', '0')
+        
+$(document).ready ->
+  window.signature.initialize()
 
-$(document).ready(function() {
-  window.signature.initialize();
+  $('button').on 'click', ->
+    window.signature.initialize()
+    setTimeout( ->
+      window.signature.animate()
+    , 500)
 
-  $('button').on('click', function() {
-    window.signature.initialize();
-    setTimeout(function() {
-      window.signature.animate();
-    }, 500);
-  });
-});
-
-$(window).on('load', function() {
-  window.signature.animate();
-});
+$(window).load ->
+  window.signature.animate()
